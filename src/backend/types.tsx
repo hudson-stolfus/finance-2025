e2f314821be7c5fd2c274c1838f66e9e0b5a7ad5
Hudson Stolfus <hudson.stolfus@gmail.com>
2025-01-04 21:10:33 -0600

    Initial commit

--- src/backend/types.tsx
+++ src/backend/types.tsx
@@ -1,12 +1,12 @@
-type transaction = {
-    id: number;
-    type: "income"|"expense",
-    amount: number,
-    date: Date,
-    category: string,
-}
-
-type balance = {
-    currentBalance: number,
-    lastBalance: number,
-}
+// type transaction = {
+//     id: number;
+//     type: "income"|"expense",
+//     amount: number,
+//     date: Date,
+//     category: string,
+// }
+//
+// type balance = {
+//     currentBalance: number,
+//     lastBalance: number,
+// }
