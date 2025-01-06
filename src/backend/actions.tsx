7cbb69925be1701052c67ae1746126adca9419b1
Hudson Stolfus <hudson.stolfus@gmail.com>
2025-01-05 19:28:57 -0600

    Database interfacing added Planning/TODO improvements

---
+++ src/backend/actions.tsx
@@ -0,0 +1,45 @@
+'use server';
+import {sql} from "@vercel/postgres"
+import { Transaction } from "@/backend/types"
+// functions for sending various types of data to the database such as new transactions, edited transactions, or deleting transactions
+
+export async function seedData() {
+    await sql`
+        INSERT INTO app_state (current_balance) VALUES (0.00);
+    `
+}
+
+export async function newTransaction(Transaction: Transaction) {
+    await sql`
+        WITH new_transaction AS (
+        INSERT INTO transactions (type, amount, date, category)
+        VALUES (${Transaction.type}, ${Transaction.amount}, ${Transaction.date.toISOString().slice(0,10)}, ${Transaction.category})
+            RETURNING amount, type
+            )
+        UPDATE app_state
+        SET current_balance =
+                CASE
+                    WHEN new_transaction.type = 'income' THEN current_balance + new_transaction.amount
+                    WHEN new_transaction.type = 'expense' THEN current_balance - new_transaction.amount
+                    END
+            FROM new_transaction;
+    `
+}
+
+export async function deleteTransaction(id: string) {
+    await sql`
+        WITH deleted_transaction AS (
+        DELETE FROM transactions
+        WHERE id = ${id}
+            RETURNING amount, type
+)
+        UPDATE app_state
+        SET current_balance =
+                CASE
+                    WHEN deleted_transaction.type = 'income' THEN current_balance - deleted_transaction.amount
+                    WHEN deleted_transaction.type = 'expense' THEN current_balance + deleted_transaction.amount
+                    END
+            FROM deleted_transaction;
+`
+}
+
